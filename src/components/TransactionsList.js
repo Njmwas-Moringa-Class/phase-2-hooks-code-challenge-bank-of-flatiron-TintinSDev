19071cd2e588be59cea3bf7bcbac161690ed85d0
Martin <mmaina290@gmail.com>
2023-12-06 19:41:09 +0300

    progress

--- src/components/TransactionsList.js
+++ src/components/TransactionsList.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Transaction from "./Transaction";
 
 function TransactionsList() {
+  const [transactions, setTransactions] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  useEffect(() => {
+
+    fetch("http://localhost:8001/transactions")
+      .then((response) => response.json())
+      .then((data) => setTransactions(data))
+      .catch((error) => console.error("Error fetching transactions:", error));
+  }, []);
+
+  const filteredTransactions = transactions.filter((transaction) =>
+    transaction.description.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+  // setSearchTerm("");
   return (
     <table className="ui celled striped padded table">
       <tbody>
@@ -19,7 +33,9 @@ function TransactionsList() {
             <h3 className="ui center aligned header">Amount</h3>
           </th>
         </tr>
-        {/* render a list of <Transaction> components here */}
+        {filteredTransactions.map((transaction) => (
+          <Transaction key={transaction.id} transaction={transaction} />
+        ))}
       </tbody>
     </table>
   );
